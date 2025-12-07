f6ebd8a144a88d524384c8c1243f7b27133ad49f
TacoGlz <axeldlg1305@gmail.com>
2025-12-07 16:38:40 -0600

    React app

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
+import { useEffect } from "react";
+import api from "./Components/DataProvider/Api";
+
+function App() {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    async function loadCards() {
+      try {
+        const data = await api.getAllcards();
+        setCards(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    loadCards();
+  }, []);
+
+  async function handleUpdateCard(card) {
+    try {
+      const likeinvertido = !card.like;
+      console.log(likeinvertido);
+      await api.updateCard(card._id, likeinvertido);
+      const temp = {...card, like: likeinvertido };
+      console.log(temp);
+
+      setCards((prev) =>
+        prev.map((element) => {
+          return element._id === temp._id ? temp : element;
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async function handleDeleteCard(card) {
+    try {
+      await api.deleteCard(card._id);
+      setCards((cards) => cards.filter((c) => c._id !== card._id));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async function handleCreateCard(newCard) {
+    try {
+      const createCard = await api.createCard(newCard);
+      setCards((card) => [createCard, ...card]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Main
+          cards={cards}
+          onUpdateCard={handleUpdateCard}
+          onCreateCard={handleCreateCard}
+          onDeleteCard={handleDeleteCard}
+        />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/DataProvider/Api.js
@@ -0,0 +1,46 @@
+class Api {
+  constructor(baseUrl, headers) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _handserveresponse(res) {
+    if (!res.ok) {
+      throw new Error(Error `: ${res.status}`);
+    }
+    return res.json();
+  }
+  updateCard(cardId,like){
+    return fetch(`${this._baseUrl}/updateCard/${cardId}`,{
+      method:"PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        like
+      })
+    }).then(this._handserveresponse)
+  }
+  createCard(data) {
+  return fetch(`${this._baseUrl}/createCard`, {
+    method: "POST",
+    headers: this._headers,
+    body: JSON.stringify(data),
+  }).then(this._handserveresponse);
+}
+  deleteCard(cardId){
+    return fetch(`${this._baseUrl}/deleteCard/${cardId}`,{
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handserveresponse)
+  }
+  getAllcards() {
+    return fetch(`${this._baseUrl}/getAllCards`, {
+      headers: this._headers,
+    }).then(this._handserveresponse);
+  }
+}
+
+const api = new Api("https://api-requests-lcy6.onrender.com", {
+  "Content-Type": "application/json",
+});
+
+export default api;
